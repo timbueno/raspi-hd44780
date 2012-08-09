e8d1ff352ebfbba5f07b6c002105528cab141b07
Timothy Bueno <timbueno@gmail.com>
2012-08-09 01:52:55 -0400

    LCD initialization documentation

--- hd44780.py
+++ hd44780.py
@@ -5,29 +5,34 @@
 #
 # self.cmd(command)
 #
-# 0x0C    Turns ON the LCD, no cursor
-# 0x08    Turns OFF the LCD
-# 0x0E    Turns on the LCD with a SOLD cursor
-# 0x0F    Turns on the LCD with a BLINK cursor
+#    0x0C    Turns ON the LCD, no cursor
+#    0x08    Turns OFF the LCD
+#    0x0E    Turns on the LCD with a SOLD cursor
+#    0x0F    Turns on the LCD with a BLINK cursor
 # 
 # Positions within line can be set
 # by adding an offset
 # example: 0x82 moves the cursor to
 # the third position on the first line
 #
-# 0x80    Moves cursor to LINE 1
-# 0xC0    Moves cursor to LINE 2
-# 0x94    Moves cursor to LINE 3
-# 0xD4    Moves cursor to LINE 4
+#    0x80    Moves cursor to LINE 1
+#    0xC0    Moves cursor to LINE 2
+#    0x94    Moves cursor to LINE 3
+#    0xD4    Moves cursor to LINE 4
 #
 #
-# Reference: http://joshuagalloway.com/lcd.html
+# Reference:
+#        http://joshuagalloway.com/lcd.html
+#        http://en.wikipedia.org/wiki/Hitachi_HD44780_LCD_controller
+#        http://letsmakerobots.com/node/4240
 
 
 import RPi.GPIO as GPIO
 from time import sleep
 
+#
 # 4 Bit Mode 
+#
 class HD44780:
 
     def __init__(self, pin_rs=24, pin_e=23, pins_db=[4, 17, 21, 22]):
@@ -36,24 +41,28 @@ class HD44780:
         self.pin_e = pin_e
         self.pins_db = pins_db
 
-        # Set pinmodes 
+        # Set pinmodes (All output)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin_e, GPIO.OUT)
         GPIO.setup(self.pin_rs, GPIO.OUT)
         for pin in self.pins_db:
             GPIO.setup(pin, GPIO.OUT)
 
+        # Initialize the LCD
         self.clear()
 
+    #
+    # Initialize and clear lcd
+    #
     def clear(self):
         """ Blank / Reset LCD """
 
-        self.cmd(0x33) # $33 8-bit mode
-        self.cmd(0x32) # $32 8-bit mode
-        self.cmd(0x28) # $28 8-bit mode
-        self.cmd(0x0C) # $0C 8-bit mode
-        self.cmd(0x06) # $06 8-bit mode
-        self.cmd(0x01) # $01 8-bit mode
+        self.cmd(0x33) # Initialize lcd 
+        self.cmd(0x32) # Four-bit mode
+        self.cmd(0x28) # Set interface, 2 Lines
+        self.cmd(0x0C) # Turn on Display
+        self.cmd(0x06) # Set cursor move direction right
+        self.cmd(0x01) # Clear and home display
 
     #
     # 4-Bit Write Sequence
@@ -94,6 +103,9 @@ class HD44780:
         GPIO.output(self.pin_e, True)
         GPIO.output(self.pin_e, False)
 
+    #
+    # Write string to LCD
+    #
     def message(self, text):
         """ Send string to LCD. Newline wraps to second line"""
 
