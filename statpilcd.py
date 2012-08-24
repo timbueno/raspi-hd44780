d7fe3178a2616429fa49183cdb38f68c6c0d7de1
Timothy Bueno <timbueno@gmail.com>
2012-08-23 21:32:21 -0400

    indent error

--- statpilcd.py
+++ statpilcd.py
@@ -6,22 +6,22 @@ from subprocess import *
 from time import sleep
 
 def get_ip():
-	cmd = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+    cmd = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1"
 
-	p = Popen(cmd, shell=True, stdout=PIPE)
-	output = p.communicate(0)
-	return output
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate(0)
+    return output
 
 def blinkMessage(tList, lcd):
-	# tList is a list containing tuples
-	# consisting of both lines on the display
-	while True:
-		for screen in tList:
-			for line in screen:
-				lcd.message(line)
-				if line[1] != line:
-					lcd.cmd(0xC0)
-			sleep(2)
+    # tList is a list containing tuples
+    # consisting of both lines on the display
+    while True:
+        for screen in tList:
+            for line in screen:
+                lcd.message(line)
+                if line[1] != line:
+                    lcd.cmd(0xC0)
+            sleep(2)
 
 if __name__ == '__main__':
 
@@ -30,9 +30,9 @@ if __name__ == '__main__':
     ip = get_ip()
     ip = ip[0].rstrip()
 
-	tList = [('First Line','Second Line'),('Third Line','Fourth Line'),('Fifth Line', 'Sixth Line')]
+    tList = [('First Line','Second Line'),('Third Line','Fourth Line'),('Fifth Line', 'Sixth Line')]
 
-	blinkMessage(tList, lcd)
+    blinkMessage(tList, lcd)
 
     # message = 'IP:%s' % ip
     # lcd.message(message)
